720e0254ac3575a31d3a3495df8d3cf750473f95
Giuseppe Bianchi <gppbianchi@gmail.com>
2024-02-12 21:06:42 +0100

    fix navbar.js

--- shared/navbar.js
+++ shared/navbar.js
@@ -2,6 +2,7 @@ document
   .querySelectorAll(".nav.navbar-nav li.dropdown .dropdown-toggle")
   .forEach((el, i, drops) => {
     el.addEventListener("click", function (e) {
+      e.preventDefault()
       if (e.target.offsetParent.classList.contains("open")) {
         e.target.offsetParent.classList.remove("open");
       } else {
@@ -13,7 +14,6 @@ document
 
 document.addEventListener("click", function (e) {
   const isInsideDropdown = e.target.closest(".navbar .dropdown");
-
   if (!isInsideDropdown) {
     document.querySelectorAll(".navbar .dropdown.open").forEach(function (el) {
       el.classList.remove("open");

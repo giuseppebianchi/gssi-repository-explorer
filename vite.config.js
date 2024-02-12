3daeea73d39b3a4dc006d4416f41e993f28f49a5
Giuseppe Bianchi <gppbianchi@gmail.com>
2024-02-13 00:05:05 +0100

    fix path and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import 'dotenv/config'
 
 export default defineConfig({
-  base: "/gssi-repository-explorer",
+  base: process.env.VITE_BASE_PATH,
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        graph: resolve(__dirname, "graph.html"),
-        detail: resolve(__dirname, "detail.html"),
-        barchart: resolve(__dirname, "barchart.html"),
+        graph: resolve(__dirname, "views/graph/graph.html"),
+        detail: resolve(__dirname, "views/detail/detail.html"),
+        barchart: resolve(__dirname, "views/barchart/barchart.html"),
       },
     },
   },
